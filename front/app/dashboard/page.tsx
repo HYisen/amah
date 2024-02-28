d3c88486b30332b05b1f8e34cbb54e7008b5e8a9
Alex <alexhyisen@gmail.com>
2024-02-28 23:58:34 +0800

    smooth action button switch

--- front/app/dashboard/page.tsx
+++ front/app/dashboard/page.tsx
@@ -4,12 +4,16 @@
 "use client";
 
 import {useState} from "react";
-import {Button, TextField} from "@mui/material";
+import {Button, FormControlLabel, FormGroup, Switch, TextField} from "@mui/material";
+import RefreshIcon from '@mui/icons-material/Refresh';
 import {ApplicationComplex, ApplicationLine, digestApplications, Token} from "@/app/lib/definitions";
 import {DataGrid, GridColDef, GridRenderCellParams} from "@mui/x-data-grid";
 import {humanize} from "@/app/lib/humanize";
 
-const columns = (runFunc: (appId: number) => void, killFunc: (pid: number) => void): GridColDef[] => [
+const columns = (runFunc: (appId: number) => void,
+                 killFunc: (pid: number) => void,
+                 pendingAppIds: Set<number>,
+                 pendingProcessIds: Set<Number>): GridColDef[] => [
     {
         field: "stoppable",
         headerName: "Action",
@@ -18,17 +22,25 @@ const columns = (runFunc: (appId: number) => void, killFunc: (pid: number) => vo
             return params.row.memory > 0 && params.row.pid > 0;
         },
         renderCell: (params: GridRenderCellParams<ApplicationLine, boolean>) => {
-            return params.value ?
-                <Button size="small" variant="contained" color="error"
-                        onClick={() => {
-                            killFunc(params.row.pid);
-                        }}>Kill</Button>
-                :
-                <Button size="small" variant="contained" color="success"
-                        onClick={() => {
-                            runFunc(params.row.appId);
-                        }}>Run</Button>
-                ;
+            if (params.value) {
+                if (pendingProcessIds.has(params.row.pid)) {
+                    return <Button size="small" variant="contained" color="error"><RefreshIcon/></Button>;
+                } else {
+                    return <Button size="small" variant="contained" color="error"
+                                   onClick={() => {
+                                       killFunc(params.row.pid);
+                                   }}>Kill</Button>;
+                }
+            } else {
+                if (pendingAppIds.has(params.row.appId)) {
+                    return <Button size="small" variant="contained" color="success"><RefreshIcon/></Button>;
+                } else {
+                    return <Button size="small" variant="contained" color="success"
+                                   onClick={() => {
+                                       runFunc(params.row.appId);
+                                   }}>Run</Button>;
+                }
+            }
         },
     },
     {field: "pid", headerName: "PID"},
@@ -54,8 +66,12 @@ export default function Page() {
     const [password, setPassword] = useState("");
     const [token, setToken] = useState({} as Token);
     const [applications, setApplications] = useState<ApplicationLine[]>([]);
-
+    const [updateAfterAction, setUpdateAfterAction] = useState(true);
+    const [pendingProcessIds, setPendingProcessIds] = useState<number[]>([]);
+    const [pendingApplicationIds, setPendingApplicationIds] = useState<number[]>([]);
     const host = "https://hyisen.net"
+    const updateAfterActionDelayMs: number = 500; // In practice, kill needs time to finish after DELETE is return.
+    const pendingBlockingExtensionMs: number = 500; // A bit more to prevent flash by simultaneous unblock and update.
 
     async function login() {
         const response = await fetch(`${host}/v1/session`, {
@@ -80,18 +96,40 @@ export default function Page() {
     }
 
     async function runApplication(appId: number) {
+        if (updateAfterAction) {
+            setPendingApplicationIds([...pendingApplicationIds, appId]);
+        }
         const response = await fetch(`${host}/v1/applications/${appId}/instances`,
             {method: "PUT", headers: {"Token": token.ID}});
         if (!response.ok) {
+            setPendingApplicationIds(pendingApplicationIds.filter(v => v != appId));
             alert(`bad code ${response.status}: ${await response.text()}`);
+        } else if (updateAfterAction) {
+            setTimeout(() => {
+                fetchData();
+                setTimeout(() => {
+                    setPendingApplicationIds(pendingApplicationIds.filter(v => v != appId));
+                }, pendingBlockingExtensionMs)
+            }, updateAfterActionDelayMs);
         }
     }
 
     async function killProcess(pid: number) {
+        if (updateAfterAction) {
+            setPendingProcessIds([...pendingProcessIds, pid]);
+        }
         const response = await fetch(`${host}/v1/processes/${pid}`,
             {method: "DELETE", headers: {"Token": token.ID}});
         if (!response.ok) {
+            setPendingProcessIds(pendingProcessIds.filter(v => v != pid));
             alert(`bad code ${response.status}: ${await response.text()}`);
+        } else if (updateAfterAction) {
+            setTimeout(() => {
+                fetchData();
+                setTimeout(() => {
+                    setPendingProcessIds(pendingProcessIds.filter(v => v != pid));
+                }, pendingBlockingExtensionMs)
+            }, updateAfterActionDelayMs);
         }
     }
 
@@ -110,12 +148,17 @@ export default function Page() {
                        type="password"
                        value={password}
                        onChange={event => setPassword(event.target.value)}></TextField>
+            <FormGroup>
+                <FormControlLabel control={<Switch checked={updateAfterAction}
+                                                   onChange={event => setUpdateAfterAction(event.target.checked)}/>}
+                                  label={"UpdateAfterAction"}/>
+            </FormGroup>
             <br/>
             <DataGrid columns={columns(appId => {
                 runApplication(appId).then();
             }, pid => {
                 killProcess(pid).then();
-            })} rows={applications}/>
+            }, new Set<number>(pendingApplicationIds), new Set<number>(pendingProcessIds))} rows={applications}/>
         </>
     );
 }
