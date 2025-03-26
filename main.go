602fc73ec0a712530229ea766de25f593112fc6b
Alex <alexhyisen@gmail.com>
2025-03-26 18:26:32 +0800

    add ai proxy

--- main.go
+++ main.go
@@ -34,12 +34,21 @@ var newUsername = flag.String("newUsername", "", "the new username to generate s
 var newPassword = flag.String("newPassword", "", "the new password to generate shadow line to append")
 
 func NewProxy(basic *url.URL, other *url.URL) *httputil.ReverseProxy {
+	aiagent, err := url.Parse("http://localhost:8640")
+	if err != nil {
+		log.Fatal(err)
+	}
+	aiPrefix := "/ai"
 	return &httputil.ReverseProxy{
 		Rewrite: func(r *httputil.ProxyRequest) {
 			r.SetXForwarded()
 			// I have searched it in Eta0, the v1 prefix algorithm shall work. Expand it if this becomes more complex.
 			if strings.HasPrefix(r.In.URL.Path, "/v1") {
 				r.SetURL(basic)
+			} else if strings.HasPrefix(r.In.URL.Path, aiPrefix) {
+				r.Out.URL.Path = strings.TrimPrefix(r.Out.URL.Path, aiPrefix)
+				r.Out.URL.RawPath = strings.TrimPrefix(r.Out.URL.RawPath, aiPrefix)
+				r.SetURL(aiagent)
 			} else {
 				r.SetURL(other)
 			}
