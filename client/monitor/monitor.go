HYisen/amah

commit 967154de74e3c420151c47c8919caeba2ee651b6

improve the implementation of monitor.Client.Kill
Not tested yet, but I trust the simple official API.

client/monitor/monitor.go
@@ -1,12 +1,9 @@
 package monitor
 
 import (
-	"errors"
-	"fmt"
 	"github.com/prometheus/procfs"
-	"os/exec"
-	"strconv"
-	"strings"
+	"os"
+	"syscall"
 )
 
 type Client struct {
@@ -37,17 +34,12 @@ func (c *Client) Scan() ([]Process, error) {
 
 // Kill kills the process by PID, if no such PID, would return false found and nil err.
 func (c *Client) Kill(PID int) (found bool, err error) {
-	if _, err = exec.Command("kill", strconv.Itoa(PID)).Output(); err != nil {
-		var e *exec.ExitError
-		if errors.As(err, &e) {
-			msg := string(e.Stderr)
-			if strings.HasSuffix(msg, " failed: No such process\n") {
-				return false, nil
-			}
-			return false, fmt.Errorf("stderr[%v]: %v", msg, e)
-		} else {
-			return false, err
-		}
+	// > On Unix systems, FindProcess always succeeds and returns a Process for the given pid,
+	// At present I only test and guarantee user experience on Unix systems,
+	// so we treat it as it is Unix and follow the guide in docs of os.FindProcess.
+	process, _ := os.FindProcess(PID)
+	if process.Signal(syscall.Signal(0)) != nil {
+		return false, nil
 	}
-	return true, nil
+	return true, process.Kill()
 }

commit 95d0c7e62f7507769ed81fd4e26e89ced1b206c5

change Client.Kill to gracefully shutdown
It rollbacks the feature/bug introduced in commit 967154de, as previously the invoke of kill sends SIGTERM rather than SIGKILL as default.

I prefer to allow a graceful shutdown, because I have never witnessed a program, unless intended, that does not respect SIGTERM before.

client/monitor/monitor.go
@@ -41,5 +41,5 @@ func (c *Client) Kill(pid int) (found bool, err error) {
 	if process.Signal(syscall.Signal(0)) != nil {
 		return false, nil
 	}
-	return true, process.Kill()
+	return true, process.Signal(syscall.SIGTERM)
 }